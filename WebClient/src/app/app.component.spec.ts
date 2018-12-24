afb3490009f6998e1316303b7eb7378d45d409d9
Stefan van Raaphorst <stefan.van.raaphorst@gmail.com>
2018-12-24 09:51:24 +0100

    Component App: add top bar
    - Removed template stuff
    - Add top bar
    - Add basic css

--- WebClient/src/app/app.component.spec.ts
+++ WebClient/src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('DartsToday');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render game type', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to DartsToday!');
+    expect(compiled.querySelector('.game').textContent).toContain('DartsToday');
   });
 });
