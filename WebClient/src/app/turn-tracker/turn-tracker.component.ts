c21ad214d13832a86d108ed503d5a22ec7958620
Stefan van Raaphorst <stefan.van.raaphorst@gmail.com>
2018-12-28 16:07:43 +0100

    Lib aggregate: add test (extracted from orginal development)

--- WebClient/src/app/turn-tracker/turn-tracker.component.ts
+++ WebClient/src/app/turn-tracker/turn-tracker.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { GameService, ServiceState } from '../services/game.service'
-import { DartState } from '../DartsToday/GameState';
 
 @Component({
   selector: 'app-turn-tracker',
