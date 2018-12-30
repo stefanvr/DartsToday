e7b56856acd03026fe342189be23ac6b8ba97234
Stefan van Raaphorst <stefan.van.raaphorst@gmail.com>
2018-12-30 22:52:04 +0100

    Cricket: add test game scenarios

---
+++ WebClient/src/app/DartsToday/CricketGameExamples.ts
@@ -0,0 +1,37 @@
+export const PLAYER1 = {"name":"player 1"}
+export const PLAYER2 = {"name":"player 2"}
+
+export const PLAYER1_WIN_GAME = [
+    {"action":"initialized","createdAt":"2018-12-30T20:00:24.299Z"},
+    {"action":"addPlayer","player": PLAYER1},
+    {"action":"addPlayer","player": PLAYER2},
+    {"action":"startGame","startedAt":"2018-12-30T20:00:24.301Z"},
+    {"action":"score","score":20,"multiplier":3},
+    {"action":"score","score":19,"multiplier":3},
+    {"action":"score","score":18,"multiplier":3},
+    {"action":"endTurn"},
+    {"action":"endTurn"},
+    {"action":"score","score":17,"multiplier":3},
+    {"action":"score","score":16,"multiplier":3},
+    {"action":"score","score":15,"multiplier":3},
+    {"action":"endTurn"},
+    {"action":"endTurn"},
+    {"action":"score","score":25,"multiplier":2},
+    {"action":"score","score":25,"multiplier":1},
+    {"action":"endTurn"}
+];
+
+// 20 Closed, player 1: 19 double, 18 single score
+export const GAME_STATES_GAME_ROUND = 2;
+export const GAME_STATES_GAME = { events: [
+    {"action":"initialized","createdAt":"2018-12-30T20:00:24.299Z"},
+    {"action":"addPlayer","player": PLAYER1},
+    {"action":"addPlayer","player": PLAYER2},
+    {"action":"startGame","startedAt":"2018-12-30T20:00:24.301Z"},
+    {"action":"score","score":20,"multiplier":3},
+    {"action":"endTurn"},
+    {"action":"score","score":20,"multiplier":3},
+    {"action":"endTurn"},
+    {"action":"score","score":19,"multiplier":2},
+    {"action":"score","score":18,"multiplier":1}
+]};
