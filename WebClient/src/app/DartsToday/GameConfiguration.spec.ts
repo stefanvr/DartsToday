4bb094be47c50067097e122d3747cf72aa7c79fe
Stefan van Raaphorst <stefan.van.raaphorst@gmail.com>
2019-01-03 21:35:06 +0100

    GameConfiguration: add Cricket game with 2 players as game to start.

---
+++ WebClient/src/app/DartsToday/GameConfiguration.spec.ts
@@ -0,0 +1,33 @@
+import { GameConfiguration, ActionsGameConfig } from './GameConfiguration'
+
+import { Aggregate } from '../lib/Aggregate';
+
+describe('GameConfiguration', () => {
+    let sut: Aggregate;
+    let createDate = new Date(Date.now()).toISOString();
+    
+    describe('For a new configuration:', () => {
+        beforeEach(() => {
+            sut = Aggregate.CreateNew(createDate, GameConfiguration);
+        });
+
+        it('The game state, createAt is set', () => {
+            expect(sut.state().createdAt).toBe(createDate);
+        });
+
+        it('Possible action(s): addPlayer', () => {
+            expect(sut.enabledActions()).toContain(ActionsGameConfig.start); 
+            expect(sut.enabledActions().length).toBe(1); 
+        });
+    });
+
+    describe('Current fixed cricket configuration:', () => {
+        it('The selectedGameType is "Cricket"', () => {
+            expect(sut.state().selectedGameType).toBe("Cricket");
+        });
+
+        it('The selectedPlayers are Player 1 and 2', () => {
+            expect(sut.state().selectedPlayers).toEqual([{ name: 'player 1'}, { name: 'player 2'}]);
+        });
+    });
+});
---
+++ WebClient/src/app/DartsToday/GameConfiguration.ts
@@ -0,0 +1,27 @@
+import { ActionObject } from '../lib/Aggregate';
+
+export enum ActionsGameConfig { undo/*==CMD_UNDO*/, start }
+
+export class GameConfiguratieState {
+  createdAt : null;
+  selectedGameType = "Cricket";
+  selectedPlayers = [{ name: 'player 1'}, { name: 'player 2'}];
+}
+
+export class GameConfiguration implements ActionObject {
+    public enabledActions: any[] = [];
+    public state = new GameConfiguratieState();
+
+    convertAction(command) {
+        return ActionsGameConfig[command];
+    }
+
+    initialized(event) {  
+        this.state.createdAt = event.createdAt;
+        this.enabledActions = [ActionsGameConfig.start];
+    }
+
+    start(event) {
+
+    }
+}
