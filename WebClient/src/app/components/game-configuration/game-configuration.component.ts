18b4879fc947e7f8e6a6d28b293c5926716a0855
Stefan van Raaphorst <stefan.van.raaphorst@gmail.com>
2019-01-03 11:01:41 +0100

    Setup: poc - remove loading scenario

--- WebClient/src/app/components/game-configuration/game-configuration.component.ts
+++ WebClient/src/app/components/game-configuration/game-configuration.component.ts
@@ -19,13 +19,13 @@ export class GameConfigurationComponent {
   start() {
     let player1 = { name: 'player 1'};
     let player2 = { name: 'player 2'};
-    /*
+    
     this.gameService.intializeNew(Aggregate.CreateNew(new Date(Date.now()).toISOString(), Cricket));
     this.gameService.execute({action: ActionsCricket.addPlayer, player: player1});
     this.gameService.execute({action: ActionsCricket.addPlayer, player: player2});
     this.gameService.execute({action: ActionsCricket.startGame, startedAt: new Date(Date.now()).toISOString()});
-    */
-   this.gameService.executeScenario(GAME_STATES_GAME);
+    
+    //this.gameService.executeScenario(GAME_STATES_GAME);
    
     this.display = "hide";
   }
