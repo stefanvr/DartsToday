962fd585dd3c530dac61d4882debfe4cd551d7e9
Stefan van Raaphorst <stefan.van.raaphorst@gmail.com>
2019-01-09 23:58:54 +0100

    Component Statistics: add basic wiring test

--- WebClient/src/app/game/statistics/statistics.component.spec.ts
+++ WebClient/src/app/game/statistics/statistics.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import * as DateTime from '../../lib/datetime'
+import { Dispatcher } from '../../lib/dispatcher'
 
-import { StatisticsComponent } from './statistics.component';
+import { StatisticsComponent,  } from './statistics.component';
+import { StatisticsService } from '../game.component'
+import { Statistics } from '../../DartsToday/Statistics'
+import { PLAYER1 } from 'src/app/DartsToday/CricketGame.Examples';
 
 describe('StatisticsComponent', () => {
   let component: StatisticsComponent;
@@ -19,7 +24,27 @@ describe('StatisticsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Initial state:', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('No players, players to be []', () => {   
+      expect(component.players).toEqual([]);
+    });
+  });
+
+  describe('With addPlayer event received:', () => {
+    beforeEach(() => {
+      let statisticsService = TestBed.get(StatisticsService);
+      statisticsService.intializeNew(DateTime.now() , Statistics);
+      let dispatcher = TestBed.get(Dispatcher);
+      dispatcher.publish({action: "addPlayer", player: PLAYER1})
+      fixture.detectChanges();
+    });
+
+    it('Single players, players to be []', () => {   
+      expect(component.players.length).toEqual(1);
+    });
   });
 });
