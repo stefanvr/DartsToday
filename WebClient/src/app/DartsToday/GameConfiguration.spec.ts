0d151202e771305f7121dd3b78f76fe7ab4f81cb
Stefan van Raaphorst <stefan.van.raaphorst@gmail.com>
2019-01-29 22:01:46 +0100

    DartsToday: reimplementation of game configuration
    - Remove dependency on custom aggregate interface
    - Take function approach redux in mind, by adding seed state option

--- WebClient/src/app/DartsToday/GameConfiguration.spec.ts
+++ WebClient/src/app/DartsToday/GameConfiguration.spec.ts
@@ -1,27 +1,15 @@
-import { GameConfiguration, ActionsGameConfig } from './GameConfiguration'
-
-import  * as DateTime from '../lib/datetime';
+import { GameConfiguration } from './GameConfiguration'
 
 import { PLAYER1, PLAYER2 } from './CricketGame.examples';
 
-
 describe('GameConfiguration', () => {
     let sut: GameConfiguration;
-    let creationDate = DateTime.now();
 
     beforeEach(() => {
         sut = new GameConfiguration();
     });
 
     describe('For a new configuration:', () => {
-        it('convertAction, converts enum ActionsPlayers to string', () => {  
-            expect(sut.convertAction(ActionsGameConfig.start)).toEqual("start");
-        });
-        
-        it('createdAt, returns null', () => {
-            expect(sut.state.createdAt).toEqual(null);
-        });
-
         it('selectedGameType, return  "Cricket"', () => {
             expect(sut.state.selectedGameType).toBe("Cricket");
         });
@@ -29,20 +17,5 @@ describe('GameConfiguration', () => {
         it('selectedPlayers, returns Player 1 and 2', () => {
             expect(sut.state.selectedPlayers).toEqual([PLAYER1, PLAYER2]);
         });   
-    });
-
-    describe('After initialized, reflects current fixed cricket configuration:', () => {
-        beforeEach(() => {
-            sut.initialized({ createdAt: creationDate });
-        });
-
-        it('createdAt, returns creation date', () => {
-            expect(sut.state.createdAt).toBe(creationDate);
-        });
-
-        it('Possible action(s): addPlayer', () => {
-            expect(sut.enabledActions).toContain(ActionsGameConfig.start); 
-            expect(sut.enabledActions.length).toBe(1); 
-        });
     });
 });
--- WebClient/src/app/DartsToday/GameConfiguration.ts
+++ WebClient/src/app/DartsToday/GameConfiguration.ts
@@ -1,21 +1,8 @@
-export enum ActionsGameConfig { undo/*==CMD_UNDO*/, start }
-
 export class GameConfiguratieState {
-  createdAt = null;
   selectedGameType = "Cricket";
   selectedPlayers = [{ id:"1", name: 'player 1'}, { id:"2", name: 'player 2'}];
 }
 
 export class GameConfiguration {
-    public enabledActions: any[] = [];
     public state = new GameConfiguratieState();
-
-    convertAction(command) {
-        return ActionsGameConfig[command];
-    }
-
-    initialized(event) {  
-        this.state.createdAt = event.createdAt;
-        this.enabledActions = [ActionsGameConfig.start];
-    }
 }
